2b76f3cc50a5b45aac5dc1df86b30e78f44d3c2b
Student007 <Student@VTC1-PC>
2023-06-04 18:51:42 +0400

    Add project files.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            int Count = 0;
+            int number2 = number;
+            int Gumar = 0;
+            while (number2 > 0)
+            {
+                number2 = number2 / 10;
+                Count++;
+            }
+            number2 = number;
+            for(int i = 0; i < Count; i++)
+            {
+                Gumar = Gumar + (astijan(number2%10,Count));
+                number2 = number2 / 10;
+            }
+            bool result = Gumar == number;
+            Console.WriteLine(result);
+
+        }
+        static int astijan(int a,int b)
+        {
+            int z = 1;
+            while (b > 0)
+            {
+                z = z * a;
+                b--;
+            }
+            return z;
+        }
+    }
+}
